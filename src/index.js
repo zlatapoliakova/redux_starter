31bacb9c28c0e8d69b2150c805dcd5843a836690
zlatapolikova <poliakova.zlata@student.uzhnu.edu.ua>
2025-06-09 22:05:57 +0300

    #3/ connect React through connect()

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createStore, bindActionCreators} from 'redux';
+import {createStore} from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import {Provider} from 'react-redux';
+
+import App from './components/App';
 
 const store = createStore(reducer);
-
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  return document.getElementById("counter").textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener('click', inc);
-
-document.getElementById("dec").addEventListener('click', dec);
-
-document.getElementById("rnd").addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <></>
+      <Provider store={store}>
+        <App />
+      </Provider>
   </React.StrictMode>
 );
