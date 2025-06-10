7bc9e1c22ee16c95ab0cc3fece7721cbb0295290
zlatapolikova <poliakova.zlata@student.uzhnu.edu.ua>
2025-06-10 18:44:06 +0300

    #4/ connect React through hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,19 @@
-import { connect } from "react-redux";
-import * as actions from "../actions";
+import { useDispatch, useSelector} from "react-redux";
+import {inc, dec, rnd} from "../actions";
 
-const Counter = ({counter, inc, dec, rnd}) => {
+const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return(
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
         </div>
     );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
-
-export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
